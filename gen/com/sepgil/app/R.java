ea4cc59f7978566473839dd8d4a0a6619aff3053
sepgil <sep.gil@gmail.com>
2011-11-07 16:09:44 +0100

    UI: Added buttons and changed time label sizes to medium.

--- gen/com/sepgil/app/R.java
+++ gen/com/sepgil/app/R.java
@@ -17,16 +17,35 @@ public final class R {
         public static final int textfield=0x7f020003;
     }
     public static final class id {
-        public static final int btnAdd=0x7f050003;
-        public static final int btnSub=0x7f050005;
-        public static final int lblMilis=0x7f050009;
-        public static final int lblMinute=0x7f050002;
-        public static final int lblSecond=0x7f050007;
-        public static final int lytInterval=0x7f050000;
-        public static final int lytIntervalMilis=0x7f050008;
-        public static final int lytIntervalMinute=0x7f050001;
-        public static final int lytIntervalSecond=0x7f050006;
-        public static final int txtEdit=0x7f050004;
+        public static final int Button01=0x7f050006;
+        public static final int Button02=0x7f050004;
+        public static final int Button03=0x7f050019;
+        public static final int EditText01=0x7f050005;
+        public static final int LinearLayout01=0x7f050007;
+        public static final int LinearLayout02=0x7f050009;
+        public static final int LinearLayout03=0x7f050002;
+        public static final int LinearLayout04=0x7f050001;
+        public static final int TextView01=0x7f05000a;
+        public static final int TextView02=0x7f050003;
+        public static final int TextView03=0x7f050008;
+        public static final int TextView04=0x7f05000b;
+        public static final int btnAdd=0x7f05000f;
+        public static final int btnSub=0x7f050011;
+        public static final int button1=0x7f05001b;
+        public static final int button2=0x7f05001c;
+        public static final int lblMilis=0x7f050015;
+        public static final int lblMinute=0x7f05000e;
+        public static final int lblSecond=0x7f050013;
+        public static final int lytIntervalMilis=0x7f050014;
+        public static final int lytIntervalSecond=0x7f050012;
+        public static final int lytRelease=0x7f05000c;
+        public static final int lytReleaseMinute=0x7f05000d;
+        public static final int tableLayout1=0x7f050016;
+        public static final int tableRow1=0x7f050017;
+        public static final int tableRow2=0x7f05001a;
+        public static final int textView1=0x7f050000;
+        public static final int toggleButton1=0x7f050018;
+        public static final int txtEdit=0x7f050010;
     }
     public static final class layout {
         public static final int main=0x7f030000;
